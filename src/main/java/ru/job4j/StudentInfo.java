c31e7a06d5fae177f415e7be0e4d7e224013e738
Konstantin <baravozuleon@gmail.com>
2023-02-02 18:22:30 +0800

    3.2. Загрузка файлов на github [#243562 #387369]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,18 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Konstantin Kostilov");
+        System.out.println("31.12.1980");
+        System.out.println("1 * 0 = 0");
+        System.out.println("1 * 1 = 1");
+        System.out.println("1 * 2 = 2");
+        System.out.println("1 * 3 = 3");
+        System.out.println("1 * 4 = 4");
+        System.out.println("1 * 5 = 5");
+        System.out.println("1 * 6 = 6");
+        System.out.println("1 * 7 = 7");
+        System.out.println("1 * 8 = 8");
+        System.out.println("1 * 9 = 9");
+    }
+}
